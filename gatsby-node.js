c7f943929cebab9cb95da80f64296991d7811fde
Rafael Haertel <rafael.haertel@nave.rs>
2019-09-24 12:25:43 -0300

    refactor: dotenv
    Signed-off-by: Rafael Haertel <rafael.haertel@nave.rs>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,11 @@
 const path = require('path')
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    node: {
+      fs: 'empty'
+    }
+  })
+}
 
 exports.createPages = ({ graphql, actions: { createPage } }) =>
   new Promise((resolve, reject) => {
@@ -29,31 +36,16 @@ exports.createPages = ({ graphql, actions: { createPage } }) =>
         `
       ).then(result => {
         if (result.errors) {
-          console.log(result.errors)
           return reject(result.errors)
         }
-
         const entries = result.data.stories.edges
-        const contents = entries.filter(entry => entry.node.field_component !== 'global_navi')
 
-        contents.forEach(entry => {
+        entries.forEach(entry => {
           const pagePath = entry.node.full_slug === 'home' ? '' : `${entry.node.full_slug}/`
-          const [globalNavi] = entries.filter(
-            globalEntry =>
-              globalEntry.node.field_component === 'global_navi' && globalEntry.node.lang === entry.node.lang
-          )
-
-          if (!globalNavi) {
-            throw new Error(
-              'The global navigation item has not been found. Please create a content item with the content type global_navi in Storyblok.'
-            )
-          }
-
           createPage({
             path: `/${pagePath}`,
             component: storyblokEntry,
             context: {
-              globalNavi: globalNavi.node,
               story: entry.node
             }
           })
